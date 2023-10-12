efb9e091645c9c716e120de71bff5d5f3b048124
Zuwa Ojefua <zuwaus@yahoo.com>
2023-10-12 15:37:03 -0600

    Update Flask app

--- src/modules/app.py
+++ src/modules/app.py
@@ -9,15 +9,32 @@ with open(model_filename, 'rb') as model_file:
 
 app = Flask(__name__)
 
-@app.route('/predict', methods=['POST'])
+@app.route('/predict', methods=['GET', 'POST'])
 def predict():
-    try:
-        data = request.get_json()  # Get the JSON data from the request
-        df = pd.DataFrame(data, index=[0])  # Convert JSON data to a DataFrame
-        predictions = xgb_model.predict(df)  # Make predictions using the loaded model
-        return jsonify(predictions.tolist())  # Return predictions as JSON
-    except Exception as e:
-        return jsonify(error=str(e)), 500  # Return an error message with a 500 status code
+    if request.method == 'POST':
+        try:
+            data = request.get_json()  # Get the JSON data from the request
+            df = pd.DataFrame(data, index=[0])  # Convert JSON data to a DataFrame
+            predictions = xgb_model.predict(df)  # Make predictions using the loaded model
+            return jsonify(predictions.tolist())  # Return predictions as JSON
+        except Exception as e:
+            return jsonify(error=str(e)), 500  # Return an error message with a 500 status code
+    else:
+        latitude = float(request.args.get('Latitude'))
+        population_density = float(request.args.get('Population_density'))
+        
+        # Create a DataFrame with the received data
+        data = {'Latitude': [latitude], 'Population_density': [population_density]}
+        df = pd.DataFrame(data)
+        
+        # Make predictions using the loaded model
+        predictions = xgb_model.predict(df)
+        
+        return jsonify(predictions.tolist())
+
+        # Example usage of GET method:
+        # http://localhost:5000/predict?Latitude=40.7128&Population_density=1000
+    
 
 
 if __name__ == '__main__':
